9660c788a2f6ade16ffe4ad911621395f35aa018
IamWei18 <williamchai1801@gmail.com>
2025-03-21 13:21:14 +0800

    Add homepage HTML structure

---
+++ scratch.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = 'Hello! Welcome to My Project!';
+    console.log('Button clicked - message shown');
+}
+
+// Initial console message
+console.log('Script loaded successfully');
